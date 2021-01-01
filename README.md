e95d54a9588a72df565ab0b6d64740f600bdfe51
hbjORbj <sldisek783@gmail.com>
2021-01-01 17:12:03 +0000

    Update README

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # Development of my own cryptocurrency and ICO on Ethereum
 
 > Token's name is "Benny Token", and it is an ERC-20 token: meaning that it follows the rules all Ethereum-based tokens must follow and it is issued on the Ethereum network instead of running on its own blockchain.
+> Token built without using libraries (i.e. OpenZeppelin)
--- contracts/BennyToken.sol
+++ contracts/BennyToken.sol
@@ -9,15 +9,14 @@ contract BennyToken {
 
   event Transfer(address indexed _from, address indexed _to, uint256 _value);
 
-  // event Approval(
-  //   address indexed _owner,
-  //   address indexed _spender,
-  //   uint256 _value
-  // );
+  event Approval(
+    address indexed _owner,
+    address indexed _spender,
+    uint256 _value
+  ); 
 
   mapping(address => uint256) public balanceOf;
-
-  // mapping(address => mapping(address => uint256)) public allowance;
+  mapping(address => mapping(address => uint256)) public allowance;
 
   constructor(uint256 _initialSupply) public {
     balanceOf[msg.sender] = _initialSupply;
@@ -46,21 +45,22 @@ contract BennyToken {
   //   return true;
   // }
 
-  // function transferFrom(
-  //   address _from,
-  //   address _to,
-  //   uint256 _value
-  // ) public returns (bool success) {
-  //   require(_value <= balanceOf[_from]);
-  //   require(_value <= allowance[_from][msg.sender]);
+  // Delegated Transfer
+  function transferFrom(
+    address _from,
+    address _to,
+    uint256 _value
+  ) public returns (bool success) {
+    require(_value <= balanceOf[_from]);
+    require(_value <= allowance[_from][msg.sender]);
 
-  //   balanceOf[_from] -= _value;
-  //   balanceOf[_to] += _value;
+    balanceOf[_from] -= _value;
+    balanceOf[_to] += _value;
 
-  //   allowance[_from][msg.sender] -= _value;
+    allowance[_from][msg.sender] -= _value;
 
-  //   emit Transfer(_from, _to, _value);
+    emit Transfer(_from, _to, _value);
 
-  //   return true;
-  // }
+    return true;
+  }
 }
