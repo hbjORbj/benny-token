5146a1fab6a16f0a5326f1cbd2c0b7e85a52a9e0
hbjORbj <sldisek783@gmail.com>
2021-01-02 19:24:46 +0000

    Token Sale Part 2

--- contracts/BennyTokenSale.sol
+++ contracts/BennyTokenSale.sol
@@ -2,12 +2,14 @@
 pragma solidity >=0.4.22 <0.9.0;
 
 import "./BennyToken.sol";
+import "@openzeppelin/contracts/math/SafeMath.sol";
 
 contract BennyTokenSale {
+  using SafeMath for uint256;
   address admin; // we don't want to expose the address of admin so its visibility is not 'public'
   BennyToken public tokenContract;
   uint256 public tokenPrice;
-  //   uint256 public tokensSold;
+  uint256 public tokensSold;
 
   event Sell(address _buyer, uint256 _amount);
 
@@ -22,19 +24,21 @@ contract BennyTokenSale {
     tokenPrice = _tokenPrice;
   }
 
-  //   function multiply(uint256 x, uint256 y) internal pure returns (uint256 z) {
-  //     require(y == 0 || (z = x * y) / y == x);
-  //   }
+  function buyTokens(uint256 _numberOfTokens) public payable {
+    // Require that the contract has enough tokens
+    require(tokenContract.balanceOf(address(this)) >= _numberOfTokens);
 
-  //   function buyTokens(uint256 _numberOfTokens) public payable {
-  //     require(msg.value == multiply(_numberOfTokens, tokenPrice));
-  //     require(tokenContract.balanceOf(this) >= _numberOfTokens);
-  //     require(tokenContract.transfer(msg.sender, _numberOfTokens));
+    // Require that value is equal to the value of tokens
+    require(msg.value == _numberOfTokens.mul(tokenPrice));
 
-  //     tokensSold += _numberOfTokens;
+    // Require that a transfer is successful
+    require(tokenContract.transfer(msg.sender, _numberOfTokens));
 
-  //     emit Sell(msg.sender, _numberOfTokens);
-  //   }
+    // Keep track of number of tokens sold
+    tokensSold = tokensSold.add(_numberOfTokens);
+
+    emit Sell(msg.sender, _numberOfTokens);
+  }
 
   //   function endSale() public {
   //     require(msg.sender == admin);
--- test/BennyTokenSale.test.js
+++ test/BennyTokenSale.test.js
@@ -1,8 +1,15 @@
+const BennyToken = artifacts.require("../contracts/BennyToken.sol");
 const BennyTokenSale = artifacts.require("../contracts/BennyTokenSale.sol");
 
 contract("BennyTokenSale", (accounts) => {
+  var tokenInstance;
   var tokenSaleInstance;
-  var tokenPrice = 1000000000000000 // in wei (15 zeroes) equals 0.001 Ether
+  var admin = accounts[0];
+  var buyer = accounts[1];
+  var tokenPrice = 1000000000000000; // in wei (15 zeroes) equals 0.001 Ether
+  var tokensAvailable = 750000; // 75% of total supply
+  var numberOfTokens = 10;
+
   it("initializes the contract with the correct values", () => {
     return BennyTokenSale.deployed()
       .then((result) => {
@@ -19,6 +26,87 @@ contract("BennyTokenSale", (accounts) => {
       })
       .then((price) => {
         assert.equal(price, tokenPrice, "token price is correct");
+      });
+  });
+
+  it("facilitates purchase of tokens", () => {
+    return BennyToken.deployed()
+      .then((result) => {
+        // Grab token contract
+        tokenInstance = result;
+        return BennyTokenSale.deployed();
+      })
+      .then((result) => {
+        // Grab token sale contract
+        tokenSaleInstance = result;
+        // Provision 75% of all tokens to the token sale
+        return tokenInstance.transfer(
+          tokenSaleInstance.address,
+          tokensAvailable,
+          { from: admin }
+        );
+      })
+      .then((receipt) => {
+        return tokenSaleInstance.buyTokens(numberOfTokens, {
+          from: buyer,
+          value: numberOfTokens * tokenPrice,
+        });
+      })
+      .then((receipt) => {
+        assert.equal(receipt.logs.length, 1, "triggers one event");
+        assert.equal(
+          receipt.logs[0].event,
+          "Sell",
+          "should be the 'Sell' event"
+        );
+        assert.equal(
+          receipt.logs[0].args._buyer,
+          buyer,
+          "logs the account that purchased the tokens"
+        );
+        assert.equal(
+          receipt.logs[0].args._amount,
+          numberOfTokens,
+          "logs the number of tokens purchased"
+        );
+        return tokenSaleInstance.tokensSold();
+      })
+      .then((amount) => {
+        assert.equal(
+          amount.toNumber(),
+          numberOfTokens,
+          "increments the number of tokens sold"
+        );
+        return tokenInstance.balanceOf(buyer);
+      })
+      .then((balance) => {
+        assert.equal(balance.toNumber(), numberOfTokens);
+        return tokenInstance.balanceOf(tokenSaleInstance.address);
+      })
+      .then((balance) => {
+        assert.equal(balance.toNumber(), tokensAvailable - numberOfTokens);
+        return tokenSaleInstance.buyTokens(800000, {
+          from: buyer,
+          value: numberOfTokens * tokenPrice,
+        });
+      })
+      .then(assert.fail)
+      .catch(function (error) {
+        assert(
+          error.message.indexOf("revert") >= 0,
+          "cannot purchase more tokens than available"
+        );
+        return tokenSaleInstance.buyTokens(numberOfTokens, {
+          from: buyer,
+          value: 1,
+        });
+      })
+      .then(assert.fail)
+      .catch(function (error) {
+        assert(
+          error.message.indexOf("revert") >= 0,
+          "msg.value must equal number of tokens in wei"
+        );
       });
   });
 });
