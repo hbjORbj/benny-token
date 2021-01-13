2894c17d0c0830d9883d9a0cbbe854cb4e51e33f
hbjORbj <sldisek783@gmail.com>
2021-01-13 22:37:59 +0000

    ICO Website Done

--- src/js/app.js
+++ src/js/app.js
@@ -17,7 +17,9 @@ App = {
       App.web3Provider = web3.currentProvider;
       window.web3 = new Web3(web3.currentProvider);
     } else {
-      alert("No Ethereum interface injected into browser. Read-only access");
+      alert(
+        "You need an Ethereum interface injected into browser (i.e. Meta Mask)."
+      );
     }
     return App.initContracts();
   },
@@ -36,12 +38,40 @@ App = {
         App.contracts.BennyToken.deployed().then(function (bennyToken) {
           console.log("Benny Token Address: ", bennyToken.address);
         });
+        App.listenForEvents();
         return App.render();
       });
     });
   },
 
+  listenForEvents: function () {
+    App.contracts.BennyTokenSale.deployed().then(function (instance) {
+      instance
+        .Sell(
+          {},
+          {
+            fromBlock: 0,
+            toBlock: "latest",
+          }
+        )
+        .watch(function (error, event) {
+          console.log("Event Triggered", event);
+          App.render();
+        });
+    });
+  },
+
   render: function () {
+    if (App.loading) {
+      return;
+    }
+    App.loading = true;
+    let loader = $("#loader");
+    let content = $("#content");
+
+    loader.show();
+    content.hide();
+
     ethereum
       .enable()
       .then(function (accounts) {
@@ -50,6 +80,59 @@ App = {
       })
       .catch(function (error) {
         console.log(error);
+      });
+
+    // Load Token Sale contract
+    App.contracts.BennyTokenSale.deployed()
+      .then(function (instance) {
+        bennyTokenSaleInstance = instance;
+        return bennyTokenSaleInstance.tokenPrice();
+      })
+      .then(function (tokenPrice) {
+        App.tokenPrice = tokenPrice.toNumber();
+        $("#token-price").html(web3.fromWei(tokenPrice.toNumber(), "ether"));
+        return bennyTokenSaleInstance.tokensSold();
+      })
+      .then(function (tokensSold) {
+        App.tokensSold = tokensSold.toNumber();
+        $("#tokens-sold").html(App.tokensSold);
+        $("#tokens-remaining").html(App.tokensRemaining);
+
+        // Progress Bar
+        let progressPercent =
+          (App.tokensSold / App.tokensRemaining).toFixed(5) * 100;
+        $("#progress").css("width", progressPercent + "%");
+
+        // Load Token contract
+        App.contracts.BennyToken.deployed()
+          .then(function (instance) {
+            bennyTokenInstance = instance;
+            return bennyTokenInstance.balanceOf(App.account);
+          })
+          .then(function (balance) {
+            $("#token-balance").html(balance.toNumber());
+
+            App.loading = false;
+            loader.hide();
+            content.show();
+          });
+      });
+  },
+
+  buyTokens: function () {
+    $("#content").hide();
+    $("#loader").show();
+    let numberOfTokens = $("#numberOfTokens").val();
+    App.contracts.BennyTokenSale.deployed()
+      .then(function (bennyTokenSale) {
+        return bennyTokenSale.buyTokens(numberOfTokens, {
+          from: App.account,
+          value: numberOfTokens * App.tokenPrice,
+          gas: 500000,
+        });
+      })
+      .then(function (result) {
+        // Wait for Sell event to be triggered
       });
   },
 };
